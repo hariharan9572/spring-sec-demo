c54c517bee93ad8a2fb74a79aed2b5466771d5d9
Hariharan <hariharan9572@gmail.com>
2026-01-16 02:31:27 +0530

    Validating token

--- src/main/java/com/hariharan/springsecdemo/service/JwtService.java
+++ src/main/java/com/hariharan/springsecdemo/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.hariharan.springsecdemo.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -14,6 +15,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -56,10 +58,32 @@ public class JwtService {
     }
 
     public String extractUserName(String token) {
-        return "";
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
     }
 
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build().parseClaimsJws(token).getBody();
+    }
+
+
     public boolean validateToken(String token, UserDetails userDetails) {
-        return true;
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
