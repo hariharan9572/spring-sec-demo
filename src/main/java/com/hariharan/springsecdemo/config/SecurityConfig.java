fe590a197f2fcb04217a42da0190616c03f905a7
Hariharan <hariharan9572@gmail.com>
2026-01-16 02:12:41 +0530

    Creating a JWT Filter

--- src/main/java/com/hariharan/springsecdemo/config/SecurityConfig.java
+++ src/main/java/com/hariharan/springsecdemo/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationProvider authProvider(){
@@ -71,7 +75,8 @@ public class SecurityConfig {
                     .anyRequest().authenticated())
                     .httpBasic(Customizer.withDefaults())
                     .sessionManagement(session ->
-                            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
