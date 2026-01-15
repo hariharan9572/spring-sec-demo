b06dba6bba9dfc05a493ea744632721f95361122
Hariharan <hariharan9572@gmail.com>
2026-01-15 19:13:30 +0530

    Error without csrf token

---
+++ src/main/java/com/hariharan/springsecdemo/StudentController.java
@@ -0,0 +1,29 @@
+package com.hariharan.springsecdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Hari", "Java"),
+            new Student(2, "Divya", "Python")
+    ));
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+
+}
