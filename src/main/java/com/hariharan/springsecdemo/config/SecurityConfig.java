9b9a5cb696fc8425656b8d4451f16a8e93bd385b
Hariharan <hariharan9572@gmail.com>
2026-01-16 01:27:19 +0530

    custom login

--- src/main/java/com/hariharan/springsecdemo/config/SecurityConfig.java
+++ src/main/java/com/hariharan/springsecdemo/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.hariharan.springsecdemo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -64,7 +66,7 @@ public class SecurityConfig {
 
         http.csrf(customizer -> customizer.disable())
                     .authorizeHttpRequests(request -> request
-                            .requestMatchers("/register")
+                            .requestMatchers("/register", "/login")
                             .permitAll()
                     .anyRequest().authenticated())
                     .httpBasic(Customizer.withDefaults())
@@ -72,6 +74,11 @@ public class SecurityConfig {
                             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
     }
 
     /*@Bean
