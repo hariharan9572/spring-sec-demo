03ff73a036a1a45225bf4777956eaa1055083e30
Hariharan <hariharan9572@gmail.com>
2026-01-15 19:20:07 +0530

    Sending CSRF Token

--- src/main/java/com/hariharan/springsecdemo/StudentController.java
+++ src/main/java/com/hariharan/springsecdemo/StudentController.java
@@ -1,5 +1,7 @@
 package com.hariharan.springsecdemo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,11 @@ public class StudentController {
             new Student(1, "Hari", "Java"),
             new Student(2, "Divya", "Python")
     ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 
     @GetMapping("students")
     public List<Student> getStudents(){
